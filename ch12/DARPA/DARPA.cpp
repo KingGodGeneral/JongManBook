63c4c7676aacb5f6a7cff79e631c50609b696491
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2017-11-15 15:14:07 +0900

    Seungwon dp

--- ch12/DARPA/DARPA.cpp
+++ ch12/DARPA/DARPA.cpp
@@ -1,36 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
-//결정 문제: 정렬되어있는 location 중 camera를 선택해 모든 카메라 간의 간격이
-//gap 이상이 되는 방법이 있는지를 반환한다.
-bool decision(const vector<double>& location, int cameras, double gap) {
-	//카메라를 설치할 수 있을 때마다 설치하는 탐욕적 알고리즘
-	double limit = -1;
-	int installed = 0;
-	for (int i = 0; i < location.size(); ++i) {
-		if (limit <= location[i]) {
-			++installed;
-			//location[i]+gap 이후는 되어야 카메라를 설치할 수 있다.
-			limit = location[i] + gap;
-		}
-	}
-	//결과적으로 cameras대 이상을 설치할 수 있었으면 성공
-	return installed >= cameras;
-}
-double optimize(const vector<double>& location, int cameras) {
-	double lo = 0, hi = 241;
-	//반복문 불변식: decision(lo)&& !decision(hi)
-	for (int it = 0; it < 100; ++it) {
-		double mid = (lo + hi) / 2.0;
-		//간격이 mid 이상이 되도록 할 수 있으면 답은 [mid,hi]에 있다.
-		if (decision(location, cameras, mid)) 
-			lo = mid;
-		else 
-			hi = mid;
-	}
-	return lo;
-}
 
 int main() {
 	cin.tie(0); ios_base::sync_with_stdio(0);
@@ -41,7 +12,20 @@ int main() {
 		for (int i = 0; i < M; i++) {
 			cin >> location[i];
 		}
-		double ans = optimize(location, N);
+		vector<vector<double>> dp(M-1, vector<double>(N-1));
+		for (int i = 0; i < M-1; i++) {
+			dp[i][0] = location[M - 1] - location[i];
+		}
+		//dp[a][b] = location[a]에 카메라를 놓고, location[M-1]에 카메라를 놓고, 
+		//그 사이에 b개의 카메라를 놓았을 때, 가장 가까운 두 카메라간 최대 간격 
+		for (int i = 1; i < N-1; i++) {
+			for (int j = 0; j < M-i-1; j++) {
+				for (int p = j+1; p < M - i; p++) {
+					dp[j][i] = max(dp[j][i], min(location[p] - location[j], dp[p][i - 1]));
+				}
+			}
+		}		
+		double ans = dp[0][N-2];
 		cout << fixed;
 		cout.precision(2);
 		cout << ans << '\n';
